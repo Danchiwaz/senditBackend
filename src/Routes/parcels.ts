d3f5b81053f82b8dfa0a811253dd99c71d29a50e
Danchiwaz <mainadanielchiwaz@gmail.com>
2022-09-14 15:07:52 +0300

    Added the middleware

--- src/Routes/parcels.ts
+++ src/Routes/parcels.ts
@@ -1,13 +1,14 @@
 import { Router } from "express";
 import { deleteParcel, getallParcels, insertParcel, updateParcel } from "../controllers/parcels";
+import { requireAuth } from "../middleware/authMiddleware";
 
 const parcelRouter = Router()
 
 
-parcelRouter.post("/", insertParcel);
-parcelRouter.get("/", getallParcels);
-parcelRouter.put("/:id", updateParcel);
-parcelRouter.delete("/:id", deleteParcel);
+parcelRouter.post("/", requireAuth, insertParcel);
+parcelRouter.get("/",requireAuth, getallParcels);
+parcelRouter.put("/:id", requireAuth, updateParcel);
+parcelRouter.delete("/:id", requireAuth, deleteParcel);
 
 
 
---
+++ src/middleware/authMiddleware.ts
@@ -0,0 +1,31 @@
+import { NextFunction, Request, Response } from "express";
+import jwt from "jsonwebtoken";
+
+export const requireAuth = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const token = req.headers.token as string;
+  if (token) {
+    jwt.verify(
+      token,
+      process.env.USER_ACCESS_TOKEN as string,
+      (err: any, decodedToken: any) => {
+        if (err) {
+          return res.json({
+            message: "You are not allowed to access this Page",
+          });
+        } else {
+         console.log(decodedToken);
+         
+          next();
+        }
+      }
+    );
+  } else {
+    return res.json({
+      message: "You are not allowed to access this Page",
+    });
+  }
+};
